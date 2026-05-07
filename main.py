cc755f9567f76b3bfaf0a901369c24e8e666d317
DiamondOsas <diamondosazuwa@gmail.com>
2026-05-07 21:42:31 +0100

    Come abck

--- main.py
+++ main.py
@@ -1,17 +1,21 @@
-from telethon.sync import TelegramClient
-from telethon.sessions import StringSession
-from datetime import date
-import requests 
 import os
+from datetime import date
+
+import requests
 from dotenv import load_dotenv
+from telethon.sessions import StringSession
+from telethon.sync import TelegramClient
 
 load_dotenv()
 
-app_id= (os.getenv('APP_ID'))
-app_hash = os.getenv('APP_HASH')
-session_string=os.getenv('SESSION_STRING')
-mummy_no = "+"
-message = f"Good Morning Ma. Welcome to {date.today()} I hope you had a good night sleep"
+app_id = os.getenv("APP_ID")
+app_hash = os.getenv("APP_HASH")
+session_string = os.getenv("SESSION_STRING")
+mummy_no = "+2348034956959"
+message = (
+    f"Good Morning Ma. Welcome to {date.today()} I hope you had a good night sleep"
+)
+
 
 def prepare_message():
     response = requests.get("https://zenquotes.io/api/today")
@@ -23,14 +27,14 @@ def prepare_message():
     else:
         print("Failed to fetch quote")
 
+
 def main():
     with TelegramClient(StringSession(session_string), app_id, app_hash) as client:
         client.send_message(mummy_no, message)
 
-            
         print("message sent sucessfully")
+
 
 if __name__ == "__main__":
     prepare_message()
     main()
-
